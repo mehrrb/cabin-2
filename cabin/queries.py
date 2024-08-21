8edaec372268f69aaab99a78535f30e389effc59
mehrab <mehrabkamli99@gmail.com>
2024-08-21 14:54:42 +0330

    fix query

--- cabin/queries.py
+++ cabin/queries.py
@@ -1,4 +1,8 @@
 from cabin.models import *
+from django.db.models import Count,Sum,F,Q
+from django.db.models import F, ExpressionWrapper, DurationField
+from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import D
 
 
 def query_0(x):
@@ -7,33 +11,38 @@ def query_0(x):
 
 
 def query_1(x):
-    q = 'your query here'
-    return q
+    
+    return Payment.objects.filter(ride__car__owner_id=x).aggregate(total_amount=sum('amount'))['total_amount']
 
 
 def query_2(x):
-    q = 'your query here'
-    return q
-
+    
+    # return Ride.objects.filter(request__rider_id=x).select_related('car')
+    return Ride.objects.filter(request__rider_id=x)
 
 def query_3(t):
-    q = 'your query here'
-    return q
+    
+    duration = ExpressionWrapper(F('dropoff_time') - F('pickup_time'), output_field=DurationField())
+    travels = Ride.objects.filter(duration__gt=t)
+
 
 
 def query_4(x, y, r):
-    q = 'your query here'
-    return q
+    
+    point = Point(x, y)
+    return Driver.objects.filter(
+        active=True,
+        location__distance_lte=(point, D(km=r))
+    )
 
 
 def query_5(n, c):
-    q = 'your query here'
-    return q
+    
+    return Driver.objects.filter(Q(car__car_type='A') | Q(car__color=c), rides__count__gte=n).distinct()
 
 
 def query_6(x, t):
-    q = 'your query here'
-    return q
+    return Rider.objects.filter(riders__count_gte=x)
 
 
 def query_7():
